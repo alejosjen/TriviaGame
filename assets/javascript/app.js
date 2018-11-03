cedacfd3dbdf7f2b13be355566e2ffdf93575b49
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-02 19:24:01 -0700

    Updated html and css to clean up styles and organization, added @media for responsive capability, started pseudocode on javascript.

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+
+// Things we need to count
+// Correct answers
+var correctAnswerCounter = 0;
+var correctArray = [];
+// Incorrect answers
+var incorrectAnswerCounter = 0;
+var incorrectArray = [];
+// Didn't answer
+var noAttemptCounter = 0;
+var noAttemptArray = [];
+var percentageCorrect = 0;
+
+// Timer to start when window is loaded
+// Time out in 4 minutes
+// Convert milliseconds to seconds to minutes
+window.onload = setTimeout(function(){
+    $('.option').onclick
+})
+
+// Assign objects from HTML to correct, incorrect, no attempt Arrays
+// Count items in array to list.length the amount of correct, incorrect, no attempts
+// Present counters after time out
+// Present percentage correct
+// Count total possible and divide by amount correct.length for percentage
+function gameTotal() {
+
+}
+
+// Reset button at end of game, no reloading page
+function reset () {
+
+}
