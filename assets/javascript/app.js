996bf7c09b2e41c994edde8aab2931701082d6d1
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-02 21:26:17 -0700

    updated JS with start button and an interval setting to toggle on and off different elements. Try again button toggles game and count down again.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,12 +11,37 @@ var noAttemptCounter = 0;
 var noAttemptArray = [];
 var percentageCorrect = 0;
 
-// Timer to start when window is loaded
-// Time out in 4 minutes
-// Convert milliseconds to seconds to minutes
-window.onload = setTimeout(function(){
-    $('.option').onclick
+//Turn questions & counters off at start of game 
+$('.col-1').toggle(false);
+$('.row-3').toggle(false);
+
+// Start button
+$(document).ready(function(){
+    $('.start').click(function () {
+    $('.start').toggle(false);
+    $('.col-1').toggle(true);
+    var number = 3;
+    intervalID = setInterval(decrement, 1000);
+    function decrement() {
+        number--;
+        $("#interval").html(number);
+     if (number === 0) {
+         clearInterval(intervalID);
+         $('.col-1').css("display", "none");
+         $('.row-3').toggle();
+     }}
+});
 })
+
+//start interval{}
+
+
+// Slide1 appears after start button clicked
+
+
+// Count down in 4 minutes
+// Convert milliseconds to seconds to minutes
+
 
 // Assign objects from HTML to correct, incorrect, no attempt Arrays
 // Count items in array to list.length the amount of correct, incorrect, no attempts
@@ -28,6 +53,17 @@ function gameTotal() {
 }
 
 // Reset button at end of game, no reloading page
-function reset () {
-
-}
+$('.try-again').click(function () {
+    $('.start').toggle(false);
+    $('.col-1').toggle(true);
+    var number = 3;
+    intervalID = setInterval(decrement, 1000);
+    function decrement() {
+        number--;
+        $("#interval").html(number);
+     if (number === 0) {
+         clearInterval(intervalID);
+         $('.col-1').toggle(false);
+         $('.row-3').toggle(true);
+     }};
+    });
